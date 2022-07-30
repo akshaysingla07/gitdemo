89c1d4fae3334e948e856e93b35735aeb807c3c8
singla1 <singla1@GGN2000482.bsci.bossci.com>
2022-07-30 13:49:46 +0530

    DB boot Commit

---
+++ 05dbboot/src/main/java/com/anubhavtrainings/service/IAddressPersistence.java
@@ -0,0 +1,11 @@
+package com.anubhavtrainings.service;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.anubhavtrainings.entities.address;
+
+
+
+public interface IAddressPersistence extends JpaRepository<address, Long> {
+
+}
---
+++ 05dbboot/src/main/resources/static/controller/Main.controller.js
@@ -0,0 +1,57 @@
+sap.ui.define(
+	["sap/ui/core/mvc/Controller",
+		"jquery.sap.global",
+		"anubhav/util/service",
+		"sap/m/MessageBox"],
+	function(Controller, jQuery, service ,MessageBox) {
+		return Controller.extend("anubhav.controller.Main", {
+			onInit: function() {
+				var oModel = new sap.ui.model.json.JSONModel();
+				oModel.setData({
+					"postPayload": {
+						"companyName": "",
+						"firstName": "",
+						"lastName": "",
+						"website": "",
+						"email": "",
+						"status": "",
+						"gstNo": "",
+					},
+					"vendor": {}
+				});
+
+				//set the model ibjecct  to the App level
+				sap.ui.getCore().setModel(oModel);
+			}
+			,
+			onSave:function(){
+				var oModel = sap.ui.getCore().getModel();
+				var payload = oModel.getProperty("/postPayload");
+				service.callService("/vendor", "POST", payload).then(function(){
+						MessageBox.confirm("Save Sucess");
+				}).catch(function(){MessageBox.error("Post failed");
+				});
+				
+			},
+			
+			onLoadData: function() {
+				//			alert("todo : we will call our service here");
+				var that = this;
+				service.callService("newVendor", "GET", {}).then(function(data) {
+					//console.log(data)
+					var oTable = that.getView().byId("idTable");
+					//create new ui5 model which contains the data
+					var oModel = sap.ui.getCore().getModel();
+					oModel.setProperty("/vendor", data._embedded.vendor);
+
+					//set the model ibjecct  to the App level
+					//sap.ui.getCore().setModel(oModel);
+					//Bind aggregation of the table
+					oTable.bindRows("/vendor");
+				}).catch(function(oError) {	
+
+				});
+
+			}
+		});
+	});
